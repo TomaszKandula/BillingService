43a45c72b794baad6f33fb10f39550a847b76c3b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-01-01 00:37:43 +0100

    feat: add database context

---
+++ BillingService.Backend/BillingService.Backend.Database/DatabaseContext.cs
@@ -0,0 +1,33 @@
+﻿namespace BillingService.Backend.Database;
+
+using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+[ExcludeFromCodeCoverage]
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    public virtual DbSet<Users> Users { get; set; }
+
+    public virtual DbSet<UserDetails> UserDetails { get; set; }
+
+    public virtual DbSet<UserDomains> UserDomains { get; set; }
+
+    public virtual DbSet<UserBillings> UserBillings { get; set; }
+
+    public virtual DbSet<UserCharges> UserCharges { get; set; }
+
+    public virtual DbSet<ChargeTypes> ChargeTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        ApplyConfiguration(modelBuilder);
+    }
+
+    private static void ApplyConfiguration(ModelBuilder modelBuilder) 
+        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+}
