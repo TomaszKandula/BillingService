246e3352478a470458cbac06435c34afeec361ed
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-12-31 23:08:40 +0100

    feat: add core implementations

---
+++ BillingService.Backend/BillingService.Backend.Core/Exceptions/ApiVersionException.cs
@@ -0,0 +1,23 @@
+namespace BillingService.Backend.Core.Exceptions;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using System.Diagnostics.CodeAnalysis;
+using Models;
+
+[ExcludeFromCodeCoverage]
+public class ApiVersionException : IErrorResponseProvider
+{
+    public IActionResult CreateResponse(ErrorResponseContext context)
+    {
+        const string errorCode = "INVALID_API_VERSION";
+        const string errorMessage = "Provided API version seems to be invalid";
+
+        var innerError = context.Message;
+        var error = new ApplicationError(errorCode, errorMessage, innerError);
+        var response = new ObjectResult(error) { StatusCode = StatusCodes.Status400BadRequest };
+
+        return response;
+    }
+}
---
+++ BillingService.Backend/BillingService.Backend.Core/Models/DateRangeValidator.cs
@@ -0,0 +1,17 @@
+namespace BillingService.Backend.Core.Models;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+[ExcludeFromCodeCoverage]
+public class DateRangeValidator
+{
+    public DateTime StartDate { get; }
+    public DateTime EndDate { get; }
+
+    public DateRangeValidator(DateTime startDate, DateTime endDate)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+}
